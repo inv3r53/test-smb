f2f101b51de9725e540242b8959ba347fc3dbc34
Suketu Dagia <suketu.dagia@mlcinsurance.com.au>
2020-04-02 21:21:15 +1100

    support accepting password from console

--- src/main/java/au/com/mlcl/integration/Main.java
+++ src/main/java/au/com/mlcl/integration/Main.java
@@ -1,5 +1,7 @@
 package au.com.mlcl.integration;
 
+import java.io.Console;
+
 import com.hierynomus.msfscc.fileinformation.FileIdBothDirectoryInformation;
 import com.hierynomus.smbj.SMBClient;
 import com.hierynomus.smbj.auth.AuthenticationContext;
@@ -10,18 +12,30 @@ import com.hierynomus.smbj.share.DiskShare;
 public class Main {
 	public static void main(String[] args) throws Exception {
 
-		// java args -  host "" username password shareName
-		// localhost "" sekhtu.dagia XXXXXXX1 share
-		if (args == null || args.length !=5) {
-			System.out.println("Must pass args: host domain user password share");
+		// java args - host "" username  shareName password
+		// localhost "" su.dg XXXXXXX1 share
+		if (args == null || args.length != 4) {
+			System.out.println("Must pass args: host domain user share [password]");
 			System.exit(1);
 		}
-		
-		String host=args[0].trim();
-		String domain=args[1].trim();
-		String user=args[2].trim();
-		String password=args[3].trim();
-		String sharePath=args[4].trim();
+		String password="";
+		String host = args[0].trim();
+		String domain = args[1].trim();
+		String user = args[2].trim();
+		String sharePath = args[3].trim();
+		if(args.length==5){
+			 password = args[3].trim();
+		}else{
+            Console console = System.console();
+
+            if (console != null) {
+                System.out.print("Enter password for " + user + ": ");
+                password = new String(console.readPassword());
+                System.out.println("----------------------------------------------------");
+            } else {
+                throw new Exception("Samba password not found.");
+            }
+        }
 		SMBClient client = new SMBClient();
 		try (Connection connection = client.connect(host)) {
 			AuthenticationContext ac = new AuthenticationContext(user, password.toCharArray(), domain);
@@ -33,7 +47,7 @@ public class Main {
 					System.out.println("File : " + f.getFileName());
 				}
 			}
-		}finally{
+		} finally {
 			client.close();
 		}
 
